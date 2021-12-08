30a8446782baf96fd52fd8e1796d3a809ec233a7
Gizem Gül Tanış <gizemgultanis@Gizem-MacBook-Air.local>
2021-12-08 21:18:20 +0300

    initial

---
+++ land-road/src/test/java/com/example/landroad/LandRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

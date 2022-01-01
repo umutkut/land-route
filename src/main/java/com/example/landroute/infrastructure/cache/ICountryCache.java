9db27161819e080278945b2cbf757a9fe9db3b7f
umututkut <umututku.tanis@gmail.com>
2022-01-01 15:55:41 +0300

    pom.xml dependency analysis

--- src/main/java/com/example/landroute/infrastructure/cache/ICountryCache.java
+++ src/main/java/com/example/landroute/infrastructure/cache/ICountryCache.java
@@ -2,8 +2,6 @@ package com.example.landroute.infrastructure.cache;
 
 import com.example.landroute.domain.Country;
 
-import java.util.Map;
-
 public interface ICountryCache {
     void cache(Country country);
 
